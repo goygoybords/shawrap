574cbe2b686a8516423c12a51d67289004673bbe
Kevin Sean Kho <kkho@ph-cfmb4m3.lrdc.lexmark.com>
2024-09-15 11:21:55 +0800

    Creating a simple layout bootstrap using the old blade system style
    -will be creating an AUTH, CRUD for each master files.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'My Laravel App')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    @include('partials.navbar')
+
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Welcome to the Home Page</h1>
+        <p class="lead">This is the home page of our Laravel app using the Blade templating engine.</p>
+    </div>
+@endsection
